945768a3831b9b7e52ea52b7786e39ebc6d5f43a
nlobo9 <nlobo9@uwo.ca>
2023-03-13 00:55:18 -0400

    Renamed Application to prevent merge future merge conflict? Add enum

---
+++ src/main/java/com/cs2212/gis_project/Category.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.cs2212.gis_project;
+
+/**
+ *
+ * @author nlobo9
+ */
+public enum Category {
+    CLASSROOM,
+    RESTAURANT,
+    LAB,
+    WASHROOM,
+    ELEVATOR
+}
