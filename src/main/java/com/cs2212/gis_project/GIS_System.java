a68545417e27c23f3c779762ded17c35d7b753ca
Rafay Kashif <kashifrafay075@gmail.com>
2023-03-15 15:58:30 -0400

    Created user and system class, Not functioning yet

---
+++ src/main/java/com/cs2212/gis_project/GIS_System.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cs2212.gis_project;
+
+/**
+ *
+ * @author kashi
+ */
+public class GIS_System {
+    
+    private static final GIS_System s_Instance = new GIS_System();
+    private User currentUser = null;
+    
+    private GIS_System() {}
+    
+    public static GIS_System getInstance()
+    {
+        return s_Instance;
+    }
+    
+    /**
+     * 
+     * @param userName
+     * @param password
+     * @return 
+     */
+    public boolean login(String userName, String password)
+    {
+        /* 
+    }
+}
