3e2e4eca6bca0679163513941168ff725fa9baa7
Rafay Kashif <kashifrafay075@gmail.com>
2023-04-01 22:35:25 -0400

    Finished load and save methods

--- src/main/java/com/cs2212/gis_project/GIS_System.java
+++ src/main/java/com/cs2212/gis_project/GIS_System.java
@@ -4,9 +4,26 @@
  */
 package com.cs2212.gis_project;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Iterator;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 /**
  *
- * @author kashi
+ * @author Rafay Kashif
  */
 public class GIS_System {
     
@@ -20,14 +37,155 @@ public class GIS_System {
         return s_Instance;
     }
     
-    /**
-     * 
-     * @param userName
-     * @param password
-     * @return 
-     */
-    public boolean login(String userName, String password)
+    public Map[] Load()
     {
-        return true;
+        try {
+            String path = "newapp.json";
+            String contents = new String(Files.readAllBytes(Paths.get(path)));
+            JSONObject buildings = new JSONObject(contents);
+            JSONObject alumni = buildings.getJSONObject("ALUMNI");
+            JSONObject middlesex = buildings.getJSONObject("MIDDLESEX");
+            JSONObject ncb = buildings.getJSONObject("NORTH_CAMPUS");
+            
+            Map[] maps = new Map[13];
+            
+            int floorLevel = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                JSONArray poiList = alumni.getJSONArray("LEVEL_" + floorLevel);
+                ArrayList<POI> pois = new ArrayList<POI>();
+                for (int j = 0; j < poiList.length(); j++)
+                {
+                    JSONObject currentPOI = poiList.getJSONObject(j);
+                    POI poi = new POI((String)currentPOI.get("name"), (Category)currentPOI.get("type"), (Integer)currentPOI.get("posX"), (Integer)currentPOI.get("posY"));
+                    pois.add(poi);
+                }
+                maps[i] = new Map("ALUMNI_" + floorLevel);
+                maps[i].setListPOI(pois);
+                floorLevel++;
+            }
+            
+            floorLevel = 1;
+            for (int i = 3; i < 8; i++)
+            {
+                JSONArray poiList = middlesex.getJSONArray("LEVEL_" + floorLevel);
+                ArrayList<POI> pois = new ArrayList<POI>();
+                for (int j = 0; j < poiList.length(); j++)
+                {
+                    JSONObject currentPOI = poiList.getJSONObject(j);
+                    POI poi = new POI((String)currentPOI.get("name"), (Category)currentPOI.get("type"), (Integer)currentPOI.get("posX"), (Integer)currentPOI.get("posY"));
+                    pois.add(poi);
+                }
+                maps[i] = new Map("MIDDLESEX_" + floorLevel);
+                maps[i].setListPOI(pois);
+                floorLevel++;
+            }
+            
+            floorLevel = 1;
+            for (int i = 8; i < 13; i++)
+            {
+                JSONArray poiList = ncb.getJSONArray("LEVEL_" + floorLevel);
+                ArrayList<POI> pois = new ArrayList<POI>();
+                for (int j = 0; j < poiList.length(); j++)
+                {
+                    JSONObject currentPOI = poiList.getJSONObject(j);
+                    POI poi = new POI((String)currentPOI.get("name"), (Category)currentPOI.get("type"), (Integer)currentPOI.get("posX"), (Integer)currentPOI.get("posY"));
+                    pois.add(poi);
+                }
+                maps[i] = new Map("NORTH_CAMPUS_" + floorLevel);
+                maps[i].setListPOI(pois);
+                floorLevel++;
+            }
+            
+            return maps;
+            
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public void Save(Map[] maps)
+    {
+        JSONObject buildings = new JSONObject();
+        JSONObject alumni = new JSONObject();
+        JSONObject middlesex = new JSONObject();
+        JSONObject ncb = new JSONObject();
+        
+        int floorLevel = 1;
+        for (int i = 0; i < 3; i++)
+        {
+            JSONArray poiList = new JSONArray();
+           for (int j = 0; j < maps[i].listPOI.size(); j++)
+           {
+               JSONObject poiData = new JSONObject();
+               POI poi = maps[i].listPOI.get(j);
+               poiData.put("name", poi.getName());
+               poiData.put("type", poi.getType());
+               poiData.put("posX", poi.getPosition()[0]);
+               poiData.put("posY", poi.getPosition()[1]);
+               
+               poiList.put(poiData);
+           }
+           
+           alumni.put("LEVEL_" + floorLevel, poiList);
+           floorLevel++;
+        }
+        
+        buildings.put("ALUMNI", alumni);
+        
+        floorLevel = 1;
+        for (int i = 3; i < 8; i++)
+        {
+             JSONArray poiList = new JSONArray();
+           for (int j = 0; j < maps[i].listPOI.size(); j++)
+           {
+               JSONObject poiData = new JSONObject();
+               POI poi = maps[i].listPOI.get(j);
+               poiData.put("name", poi.getName());
+               poiData.put("type", poi.getType());
+               poiData.put("posX", poi.getPosition()[0]);
+               poiData.put("posY", poi.getPosition()[1]);
+               
+               poiList.put(poiData);
+           }
+           
+           middlesex.put("LEVEL_" + floorLevel, poiList);
+           floorLevel++;
+        }
+        
+        buildings.put("MIDDLESEX", middlesex);
+        
+        floorLevel = 1;
+        for (int i = 8; i < 13; i++)
+        {
+             JSONArray poiList = new JSONArray();
+           for (int j = 0; j < maps[i].listPOI.size(); j++)
+           {
+               JSONObject poiData = new JSONObject();
+               POI poi = maps[i].listPOI.get(j);
+               poiData.put("name", poi.getName());
+               poiData.put("type", poi.getType());
+               poiData.put("posX", poi.getPosition()[0]);
+               poiData.put("posY", poi.getPosition()[1]);
+               
+               poiList.put(poiData);
+           }
+           
+           ncb.put("LEVEL_" + floorLevel, poiList);
+           floorLevel++;
+        }
+        
+        buildings.put("NORTH_CAMPUS", ncb);
+        
+        /* NOT THE PROPER FILE PATH */
+        try (FileWriter file = new FileWriter("app.json")) {
+            file.write(buildings.toString());
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
